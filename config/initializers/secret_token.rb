8c8e4f005661a450b04c1748c30a5386237c134a
Frank McAuley <fmcauley@resource.com>
2013-05-01 16:29:59 -0400

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Freesource::Application.config.secret_key_base = 'f47745e0651d7dc404869e9ef114354e37a7677edf2cf32cf35f80f6d754f74a3481ced2e43fc4d297d0abfb8f00c27ddd0fb0a4748d0ad4cf5f5b68a19e5caf'
